a1266d7fdb902b18b4a042c3c08b27503f973098
Oleh <81036385+petrovoleh@users.noreply.github.com>
2025-01-05 16:00:00 +0200

    Update webpack.config.js

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,5 +1,18 @@
-devServer: {
-    client: {
-        webSocketURL: 'ws://193.219.91.103:13415/ws'
-    }
-}
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        client: {
+            webSocketURL: {
+                hostname: '193.219.91.103',
+                port: 13415, // Replace with your custom port
+                protocol: 'ws',
+            },
+        },
+    },
+};
