7112ebaf01f7a137c52b651da024ebf6ba27597c
Oleh <81036385+petrovoleh@users.noreply.github.com>
2025-01-05 15:43:41 +0200

    ws support

--- src/frontend/src/config.tsx
+++ src/frontend/src/config.tsx
@@ -1,4 +1,4 @@
 export const backend_url = 'http://193.219.91.103:7438';
-export const socket = new WebSocket('ws://193.219.91.103:13415/');
+export const socket = new WebSocket('ws://193.219.91.103:13415/ws');
 
 // export const backend_url = 'http://localhost:8080';
