a535468b029231547ebf55fcc0331b6000e60781
Oleh Petrov <oleh.petrov.dev@gmail.com>
2025-12-10 11:08:45 +0200

    use origin url

--- src/frontend/src/config.tsx
+++ src/frontend/src/config.tsx
@@ -2,5 +2,5 @@
 // export const ws_url = 'ws://193.219.91.103:13415/ws'
 
 
-export const backend_url = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080";
+export const backend_url = process.env.REACT_APP_BACKEND_URL || `${window.location.origin}`;
 export const ws_url = process.env.REACT_APP_WS_URL || "";
