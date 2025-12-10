7106eabed64f3739ad3205f6c137e783c640edc9
Oleh Petrov <oleh.petrov.dev@gmail.com>
2025-12-10 11:16:36 +0200

    use origin url

--- src/backend/src/main/java/com/olehpetrov/backend/config/SecurityConfig.java
+++ src/backend/src/main/java/com/olehpetrov/backend/config/SecurityConfig.java
@@ -70,8 +70,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
                 "http://frontend:3000",
-                "http://193.219.91.103/",
-                "http://193.219.91.103:13415"
+                "http://193.219.91.103",
+                "http://193.219.91.103:*"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
