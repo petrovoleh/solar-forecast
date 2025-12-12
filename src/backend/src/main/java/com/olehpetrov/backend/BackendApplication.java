0a0fd456f38e03cc083d1dad0c842aa49f786d36
Oleh Petrov <oleh.petrov.dev@gmail.com>
2025-12-12 20:34:20 +0200

    Create admin user

--- src/backend/src/main/java/com/olehpetrov/backend/BackendApplication.java
+++ src/backend/src/main/java/com/olehpetrov/backend/BackendApplication.java
@@ -1,13 +1,25 @@
 package com.olehpetrov.backend;
 
+import com.olehpetrov.backend.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+
+    private final UserService userService;
+
+    public BackendApplication(UserService userService) {
+        this.userService = userService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        userService.createAdminUserIfNotExists();
+    }
 }
